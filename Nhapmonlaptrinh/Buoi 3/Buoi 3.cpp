d2b40e4997d6fdb87fe566fc90fa71a9e66ae2f5
Buoi 2 <Administrator@PMT2005>
2025-12-03 14:39:24 +0700

    bap tap buoi 3

---
+++ Nhapmonlaptrinh/Buoi 3/Buoi 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void cau1() {
+	const float PI = 3.14;
+	// Viet chuong trinh tinh dien tich va chu vi hinh tron voi ban kinh r
+	int r;
+	cout << "Nhap ban kinh r:  ";
+	cin >> r;
+	cout << "dien tich:  " << PI * r * r << endl;
+	cout << "chu vi:  " << 2 * PI * r << endl;
+}
+void cau2() {
+	int luong, phucap;
+	cout << "Nhap luong:  ";
+	cin >> luong;
+	cout << "Nhap phu cap:  ";
+	cin >> phucap;
+	cout << "Thuc lanh:  " << luong + phucap - (luong + phucap) * 0.1 << endl;
+}
+int main()
+{
+	cau2();
+}
+
+
