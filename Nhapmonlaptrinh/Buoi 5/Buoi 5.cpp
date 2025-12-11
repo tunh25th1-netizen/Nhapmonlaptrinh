5c38c2fb5014306e78573329ab9a751aa4df7cac
Buoi 5 <Administrator@PMT2005>
2025-12-11 12:41:04 +0700

    thuc hien xong buoi 5

--- Nhapmonlaptrinh/Buoi 5/Buoi 5.cpp
+++ Nhapmonlaptrinh/Buoi 5/Buoi 5.cpp
@@ -77,18 +77,40 @@ void cau5() {
 }
 void cau6() {
     int a, b, c;
-    cout << "Nhap a, b, c: ";
+    cout << "nhap a , b , c: ";
     cin >> a >> b >> c;
-    if (a == 0 && b == 0 && c == 0) {
-        cout << "Phuong trinh vo so nghiem" << endl;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                cout << "phuong trinh vo so nghiem" << endl;
+            }
+            else {
+                cout << "phuong trinh vo nghiem" << endl;
+            }
+        }
+        else {
+            cout << "nghiem phuong trinh: " << -c * 1.0 / b << endl;
+        }
     }
     else {
-
+        int delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            cout << " phuong trinh vo nghiem" << endl;
+        }
+        else {
+            if (delta == 0) {
+                cout << " phuong trinh co nghiem kep: " << -b * 1.0 / (2 * a) << endl;
+            }
+            else {
+                cout << " nghiem x1 = " << (-b - sqrt(delta)) / (2 * a) << endl;
+                cout << " nghiem x1 = " << (-b + sqrt(delta)) / (2 * a) << endl;
+            }
+        }
     }
 }
 int main()
 {
-    cau5();
+    cau6();
 }
 
 
