45c4e98f7d5d081a940368aeffd4bd84a691510e
Buoi 7 <Administrator@PMT2005>
2025-12-17 14:30:30 +0700

    bai tap buoi 7

--- Nhapmonlaptrinh/Buoi 7/Buoi 7.cpp
+++ Nhapmonlaptrinh/Buoi 7/Buoi 7.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-	int n = 1026;
-	bool kq = DemChuSo(n);
-	cout << "So chu so:  " << kq << endl;
+	int a = 25, b = 60;
+	int uocchunglonnhat = UCLN(a, b);
+	cout << "Uoc chung lon nhat:  " << uocchunglonnhat << endl;
 }
--- Nhapmonlaptrinh/Buoi 7/Songuyen.cpp
+++ Nhapmonlaptrinh/Buoi 7/Songuyen.cpp
@@ -13,4 +13,14 @@ int DemChuSo(int n) {
 		n /= 10;
 		dem++;
 	}
+	return dem;
+}
+int UCLN(int a, int b) {
+	while (a != b) {
+		if (a > b)
+			a -= b;
+		else
+			b -= a;
+	}
+	return a;
 }
